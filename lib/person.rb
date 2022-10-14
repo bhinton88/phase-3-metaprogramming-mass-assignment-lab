da29ba20a636808de19caefb952144e219c12a53
Bobby Hinton <bob.g.hint@gmail.com>
2022-10-14 17:00:15 -0500

    commit tree be9d980ec8d1aacb60775716b8aa21ba34ec5f06

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  # your code here
+
+  def initialize(person_attributes)
+    person_attributes.each do |key, value|
+      self.class.attr_accessor(key) # because attr_accessor is a class method.. we have to access our CLASS in order to set values for this.. hence the .class method attached to the self
+      self.send("#{key}=", value) # here we are creating the method and setting the method equal to the key name, with the value equal to the value
+    end
+  end
 end
